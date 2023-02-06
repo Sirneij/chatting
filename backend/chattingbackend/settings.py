0d005e1fbbebdd3ca5dc5a4a3fef12cd24520754
John Idogun <sirneij@gmail.com>
2023-02-06 08:34:05 +0100

    Production settings

--- backend/chattingbackend/settings.py
+++ backend/chattingbackend/settings.py
@@ -136,7 +136,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']
-STATIC_ROOT = BASE_DIR / 'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
